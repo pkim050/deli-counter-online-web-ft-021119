3076192772f71525b92ef2579bc661149a3af806
Patrick Kim <pkim050@gmail.com>
2019-02-21 07:00:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,19 @@
 # Write your code here.
 def line(array)
-  if array.size == 0
-    puts "The line is currently empty."
-  else
-    string = "The line is currently:"
-    array.each_with_index {|element, index| string << " #{index + 1}. #{element}"}
-    puts string
-  end
+  return puts "The line is currently empty." if array.size == 0
+  string = "The line is currently:"
+  array.each_with_index {|element, index| string << " #{index + 1}. #{element}"}
+  puts string
 end
 
 def take_a_number(array, string)
   array << string
-  if array.size == 1
-    puts "Welcome, #{string}. You are number 1 in line."
-  else
-    array.each_with_index do |element, index|
-      puts "Welcome, #{string}. You are number #{index + 1} in line." if index + 1 == array.size
-    end
-  end
+  return puts "Welcome, #{string}. You are number 1 in line."if array.size == 1
+  array.each_with_index {|element, index| return puts "Welcome, #{string}. You are number #{index + 1} in line." if index + 1 == array.size}
 end
 
 def now_serving(array)
-  if array.size == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{array[0]}."
-    array.shift()
-  end
+  return puts "There is nobody waiting to be served!" if array.size == 0
+  name = array.shift()
+  puts "Currently serving #{name}."
 end
